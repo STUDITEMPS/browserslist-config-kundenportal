013f4bd2cec85ced3cf1473fdb9dc317c9daf9fe
Alexander Phlippen <phlippen@mailbox.org>
2021-06-14 13:23:54 +0200

    Fix query

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ module.exports = [
   "> 0.5% in DE",
   "last 2 versions",
   "Firefox ESR",
-  "not IE 11",
-  "not dead",
-  "and supports css-variables",
-  "and supports css-grid",
-  "and supports custom-elementsv1",
+  "not IE 11 and not dead and supports css-variables and supports css-grid and supports custom-elementsv1",
 ];
