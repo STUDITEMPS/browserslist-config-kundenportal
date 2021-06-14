b5d6b56ca9519447403e4d6ce05342dd2be2a231
Alexander Phlippen <phlippen@mailbox.org>
2021-06-14 12:10:13 +0200

    Extend browser support

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = ["last 2 firefox versions", "last 2 chrome versions"];
+module.exports = [
+  "> 0.5% in DE",
+  "last 2 versions",
+  "Firefox ESR",
+  "not IE 11",
+  "not dead",
+  "supports css-variables",
+  "supports css-grid",
+  "supports custom-elementsv1",
+];
